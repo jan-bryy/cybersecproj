7c80699fc0b25b074d705466aae74bbbbc42c673
jan-bryy <concepcionjohnbry@gmail.com>
2026-09-04 13:27:49 +0800

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -32,9 +32,6 @@ import '@ionic/react/css/text-transformation.css';
 import '@ionic/react/css/flex-utils.css';
 import '@ionic/react/css/display.css';
 
-/* Ionic Dark Mode */
-import '@ionic/react/css/palettes/dark.system.css';
-
 /* Theme variables */
 import './theme/variables.css';
 
