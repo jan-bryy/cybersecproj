3f71c13d5fe4007daa02ec8591e7f75b9da6388f
jan-bryy <concepcionjohnbry@gmail.com>
2026-09-18 05:51:15 +0800

    a

---
+++ server/db.js
@@ -0,0 +1,9 @@
+// server/db.js
+const { Pool } = require('pg');
+
+const pool = new Pool({
+  connectionString: process.env.DATABASE_URL,
+  ssl: { rejectUnauthorized: false }, // Railway requires SSL
+});
+
+module.exports = pool;
